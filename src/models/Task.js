b238829122ab93ce34a4d3a5606a5699f0f2f839
dev05 <dev05@codecraftdev.com>
2022-05-25 12:55:39 -0400

    fix

--- src/models/Task.js
+++ src/models/Task.js
@@ -5,6 +5,7 @@ const schema = new db.Schema({
   _id: { type: String, required: true },
   name: { type: String },
   key: { type: String },
+  taskListId: String,
   reminderDate: Date,
   backgroundColor: String,
   createdAt: { type: Date, default: Date.now },
